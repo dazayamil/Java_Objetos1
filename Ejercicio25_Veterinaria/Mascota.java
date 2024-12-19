c52bddcfb6e228163e3be24b1ea3f3e146fe5302
Yamil Daza <dazayamil07@gmail.com>
2024-12-19 19:29:29 -0300

    Implementacion de 3 metodos, Alta consulta medica, alta vacunacion y alta guarderia

--- Ejercicio25_Veterinaria/Mascota.java
+++ Ejercicio25_Veterinaria/Mascota.java
@@ -16,4 +16,20 @@ public class Mascota {
         this.especie = especie;
         this.servicios = new ArrayList<>();
     }
+
+    public ConsultaMedica darAltaConsultaMedica(Medico medico){
+        ConsultaMedica consultaMedica = new ConsultaMedica(this, medico);
+        this.servicios.add(consultaMedica);
+        return consultaMedica;
+    }
+    public Vacunacion darAltaVacunacion(Medico medico, String nombreVacunacion, double costoVacunacion){
+        Vacunacion vacunacion = new Vacunacion(this, medico, nombreVacunacion, costoVacunacion);
+        this.servicios.add(vacunacion);
+        return vacunacion;
+    }
+    public Guarderia darAltaGuarderia(int cantidadDias){
+        Guarderia guarderia = new Guarderia(this, cantidadDias);
+        this.servicios.add(guarderia);
+        return guarderia;
+    }
 }
