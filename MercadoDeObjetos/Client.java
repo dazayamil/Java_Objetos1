a9e49309903fae8b06bee393b758b0812e0c100e
Yamil Daza <dazayamil07@gmail.com>
2025-01-28 16:47:05 -0300

    Ajustes

--- MercadoDeObjetos/Client.java
+++ MercadoDeObjetos/Client.java
@@ -5,19 +5,9 @@ import java.util.List;
 
 public class Client extends User{
     private List<Order> orders;
-    private PaymentMethod paymentMethod;
-    private ShippingMethod shippingMethod;
+
     public Client(String name, String address){
         super(name, address);
         this.orders = new ArrayList<>();
-        this.paymentMethod = null;
-        this.shippingMethod = null;
-    }
-
-    public void setPaymentMethod(PaymentMethod paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-    public void setShippingMethod(ShippingMethod shippingMethod) {
-        this.shippingMethod = shippingMethod;
     }
 }
--- MercadoDeObjetos/Order.java
+++ MercadoDeObjetos/Order.java
@@ -5,10 +5,22 @@ import java.util.List;
 
 public class Order {
     private Client client;
-    private List<Product> products;
+    private Product product;
+    private PaymentMethod paymentMethod;
+    private ShippingMethod shippingMethod;
 
-    public Order(Client client){
+    public Order(Client client, PaymentMethod paymentMethod, ShippingMethod shippingMethod, Product product){
         this.client = client;
-        this.products = new ArrayList<>();
+        this.product = product;
+        this.paymentMethod = paymentMethod;
+        this.shippingMethod = shippingMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+    public void setShippingMethod(ShippingMethod shippingMethod) {
+        this.shippingMethod = shippingMethod;
     }
 }
+
