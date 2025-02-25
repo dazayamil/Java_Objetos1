83918e9bb3c953469494b4032ef12bc8c5890c06
Yamil Daza <dazayamil07@gmail.com>
2025-02-25 17:23:09 -0300

    List, ArrayList, LinkedList & Vector - Declaration & operations

--- Extra/Colecciones/List/ExampleList.java
+++ Extra/Colecciones/List/ExampleList.java
@@ -3,14 +3,17 @@ package Extra.Colecciones.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
 public class ExampleList {
 
     /*LIST: mantienen el orden de insercion y permiten elementos duplicados, acceso por indice(directo)
     Cuando usar esto: Cuando queremos mantener un orden,acceder por posicion y que no importe tener duplicados
 
-    * ArrayList -> Mejor para acceso aleatorio y almacenamiento
-    * LinkedList -> Mejor para insercion/eliminacion frecuente
+    * ArrayList -> Mejor para acceso aleatorio y almacenamiento, basado en array dinamico,
+        bueno para Iteracion, lento para eliminacion y/o insercion.
+    * LinkedList -> Mejor para insercion/eliminacion frecuente, mejor uso en manipulacion de datos.
+        lento en acceso aleatorio
     * Vector -> Mejor para busqueda directa
     * */
     public static void main(String[] args) {
@@ -20,17 +23,41 @@ public class ExampleList {
         arrayList.add("Segundo");
         arrayList.add("Segundo"); //Permite Duplicados
 
+        //Operaciones:
+        arrayList.add("elemento"); //Agregar al final
+        arrayList.add(0, "elemento"); //Agregar en un indice
+        arrayList.get(0); // Obtener elemento de una posicion
+        arrayList.set(0, "new"); //Modificar elemento en una posicion
+        arrayList.remove(0); //Eliminar elemento por indice
+        arrayList.remove("Objeto"); //Eliminar Objeto
+        arrayList.size(); //Obtener tamaño del arrayList
+        arrayList.clear(); //Limpiar lista
+
         //LinkedList: Insercion/eliminacion rapida
         List<String> linkedList = new LinkedList<>();
         linkedList.add("Uno");
         linkedList.addFirst("Cero"); //Metodo especifico de LinkedList
         linkedList.addLast("Dos");
 
-        //Operaciones Basicas con List
-        arrayList.size(); //Obtener tamaño
-        arrayList.get(0); //Acceder por indice
-        arrayList.remove(1); //Eliminar por indice
-        arrayList.contains("primero"); //Verificar si existe dentro de la lista
-        arrayList.clear(); //Eliminar todos los elmentos
+        //Operaciones:
+        //Tiene los metodo de add, addFirst, addLast vistos anteriormente
+        linkedList.get(0); //Obtener elemento de una posicion
+        linkedList.getFirst(); //Obtener el primer elemento
+        linkedList.getLast(); //Obtener el ultimo elemento
+        linkedList.remove(0); //Eliminar por indice
+        linkedList.removeFirst(); //Eliminar el primer elemento
+        linkedList.removeLast(); //Eliminar el ultimo elemento
+
+        //Vector
+        Vector<String> vector = new Vector<>();
+
+        //Operaciones comunes:
+        vector.add("Elemento"); //agregar al final
+        vector.add(0, "Item"); //Agregar por un indice
+        vector.get(0); //Obtener elemento por un indice
+        vector.remove(0); //Eliminar por un indice
+        vector.size(); //Tamaño del vector
+        vector.capacity(); //Capacidad del vector
+        vector.trimToSize(); //Ajustar capacidad al tamaño
     }
 }
